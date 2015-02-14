814a390d7f55c58bd3f17e26455142251117c8ec
paul <paul.mouzas@gmail.com>
2015-02-14 02:38:55 -0500

    challenge 200 easy

---
+++ 200/easy.py
@@ -0,0 +1,27 @@
+image = []
+
+with open('input.txt', 'r') as f:
+    w, h = f.readline().split()
+    w, h = int(w), int(h)
+    for i in range(h):
+        image.append(list(f.readline().strip()))
+    x, y, c = f.readline().split()
+    x, y = int(x), int(y)
+
+def get_char(x, y):
+    return image[(y)%h][(x)%w]
+
+dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+
+c_to_fill = get_char(x, y)
+print c_to_fill
+print x,y
+
+def fill_image(x, y, c, c_to_fill):
+    if get_char(x, y) == c_to_fill:
+        image[(y)%h][(x)%w] = c
+        for dir_ in dirs:
+            fill_image(x+dir_[0], y+dir_[1], c, c_to_fill)
+
+fill_image(x, y, c, c_to_fill)
+print '\n'.join([''.join(row) for row in image])
