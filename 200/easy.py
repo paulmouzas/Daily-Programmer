13d571f5ccc88a0b89f8cb5cd11e69ccef01b34b
paul <paul.mouzas@gmail.com>
2015-02-14 03:44:00 -0500

    track visited nodes

--- 200/easy.py
+++ 200/easy.py
@@ -15,10 +15,15 @@ dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
 c_to_fill = get_char(x, y)
 
+visited = set()
+
 def fill_image(x, y, c, c_to_fill):
     if get_char(x, y) == c_to_fill:
         image[(y)%h][(x)%w] = c
         for dir_ in dirs:
+            if (x+dir_[0], y+dir_[1]) in visited:
+                continue
+            visited.add((x+dir_[0], y+dir_[1]))
             fill_image(x+dir_[0], y+dir_[1], c, c_to_fill)
 
 fill_image(x, y, c, c_to_fill)
